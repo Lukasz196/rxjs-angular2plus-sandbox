024016f46e09f7077cff66bdf149caab933593fb
exit196 <exit196@gmail.com>
2019-11-14 21:58:10 +0100

    pipe tap instead of new subjects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import {AppComponent} from './app.component';
 import {SubscribeChainOKComponent} from './subscribe-chain-ok/subscribe-chain-ok.component';
 import {FormsModule} from "@angular/forms";
 import {SubscribeChainBadComponent} from './subscribe-chain-bad/subscribe-chain-bad.component';
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -13,12 +14,14 @@ import {SubscribeChainBadComponent} from './subscribe-chain-bad/subscribe-chain-
     SubscribeChainOKComponent,
     SubscribeChainBadComponent
   ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        FormsModule
-    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/services/fake-rest-service.service.ts
+++ src/app/services/fake-rest-service.service.ts
@@ -1,19 +1,43 @@
 import {Injectable} from '@angular/core';
-import {Observable, of} from "rxjs";
-import {first} from "rxjs/operators";
+import {Observable, of, Subject} from "rxjs";
+import {first, tap} from "rxjs/operators";
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class FakeRestServiceService {
+  dataUrl = 'assets/data.json';
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
 
-  public getParam(): Observable<string>{
+  public getParam(): Observable<string> {
     return of('fakeData').pipe(first());
   }
 
-  public getStringDataWithParam(param: string): Observable<string>{
+  public getStringDataWithParam(param: string): Observable<string> {
     return of(`fakeData with param = ${param}`).pipe(first());
+  }
+
+  public getDataFromServer() {
+    return this.getParam()
+      .pipe(tap(receivedData => this.makeSomeAction(receivedData)));
+  }
+
+  public getDataFromServer2() {
+    const obs = new Subject();
+    const that = this;
+    this.getParam().subscribe(function (receivedData: string) {
+      that.makeSomeAction(receivedData);
+      obs.next(receivedData);
+      this.complete();
+    });
+    return obs.asObservable();
+  }
+
+
+  private makeSomeAction(receivedData: string) {
+    console.log(receivedData);
   }
 }
